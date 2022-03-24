66f7f663a84dc4c4550a0de3d75d6ab582f23a3d
shabreesh-1998 <shabreeshnair01@gmail.com>
2022-03-24 22:03:35 +0530

    Image upload

--- models/campground.js
+++ models/campground.js
@@ -3,9 +3,17 @@ const Review = require('./review');
 
 const Schema = mongoose.Schema;
 
+const imageSchema = new Schema({
+    url: String,
+    filename: String
+});
+imageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload','/upload/w_200');
+});
+
 const campgroundSchema = new Schema({
     title: String,
-    image: String,
+    images: [imageSchema],
     price: Number,
     description: String,
     location: String,
--- seeds/index.js
+++ seeds/index.js
@@ -20,10 +20,19 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '62237b6506613413831c36dc',
+            author: '6239f7a8a78d95e58838e071',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/random?collection=483251',
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/shabreeshnair/image/upload/v1648052889/YelpCamp/w00jgvraazv1gd1musng.jpg',
+                    filename: 'YelpCamp/w00jgvraazv1gd1musng'
+                },
+                {
+                    url: 'https://res.cloudinary.com/shabreeshnair/image/upload/v1648052889/YelpCamp/t0smys8t1ofm8liq3asg.jpg',
+                    filename: 'YelpCamp/t0smys8t1ofm8liq3asg'
+                }
+            ],
             description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam perspiciatis odit quis nihil ipsam tempora adipisci sint assumenda beatae in minima, nam, voluptatem debitis, inventore ducimus vitae eligendi exercitationem vel.',
             price
         })
